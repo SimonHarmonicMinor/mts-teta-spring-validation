04a5b5307a294f2b50d03401db9dd845b4bccf9d
Semyon Kirekov <semyon@kirekov.com>
2021-07-08 12:15:56 +0700

    init

---
+++ src/main/java/com/savushkin/demo/MtsTetaApplication.java
@@ -0,0 +1,13 @@
+package com.savushkin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsTetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsTetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savushkin/demo/MtsTetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savushkin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsTetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
