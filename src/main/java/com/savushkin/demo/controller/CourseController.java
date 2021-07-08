714d86393265cf69df24b7bf8ec34c39351002e6
Semyon Kirekov <semyon@kirekov.com>
2021-07-08 13:25:31 +0700

    added not found functionality

--- src/main/java/com/savushkin/demo/controller/CourseController.java
+++ src/main/java/com/savushkin/demo/controller/CourseController.java
@@ -2,13 +2,17 @@ package com.savushkin.demo.controller;
 
 import com.savushkin.demo.dao.CourseRepository;
 import com.savushkin.demo.domain.Course;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/course")
@@ -48,5 +52,12 @@ public class CourseController {
   public String deleteCourse(@PathVariable Long id) {
     courseRepository.delete(id);
     return "redirect:/course";
+  }
+
+  @ExceptionHandler
+  public ModelAndView notFoundExceptionHandler(NoSuchElementException ex) {
+    ModelAndView modelAndView = new ModelAndView("not_found");
+    modelAndView.setStatus(HttpStatus.NOT_FOUND);
+    return modelAndView;
   }
 }
