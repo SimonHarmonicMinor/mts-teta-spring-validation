f37e4c8f01c5d71564a7f05506c4573debd46ab1
Semyon Kirekov <semyon@kirekov.com>
2021-07-08 12:59:32 +0700

    Added CourseController.courseForm

--- src/main/java/com/savushkin/demo/controller/CourseController.java
+++ src/main/java/com/savushkin/demo/controller/CourseController.java
@@ -4,17 +4,28 @@ import com.savushkin.demo.dao.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/course")
 public class CourseController {
-	@Autowired
-	private CourseRepository courseRepository;
 
-	@RequestMapping
-	public String courseTable(Model model) {
-		model.addAttribute("courses", courseRepository.findAll());
-		return "course_table";
-	}
+  @Autowired
+  private CourseRepository courseRepository;
+
+  @RequestMapping
+  public String courseTable(Model model) {
+    model.addAttribute("courses", courseRepository.findAll());
+    return "course_table";
+  }
+
+  @RequestMapping("/{id}")
+  public String courseForm(Model model, @PathVariable Long id) {
+    model.addAttribute(
+    		"course",
+				courseRepository.findById(id).orElseThrow()
+		);
+    return "course_form";
+  }
 }
